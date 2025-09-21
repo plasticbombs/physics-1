17e41e5bd35e655241b12b5bc323f7e39a23ccc0
Samuel <tanuel.sam@gmail.com>
2025-09-21 14:42:22 -0400

    Exercise 1 complete, exercise 2 started

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,7 +1,3 @@
-/*
-This project uses the Raylib framework to provide us functionality for math, graphics, GUI, input etc.
-See documentation here: https://www.raylib.com/, and examples here: https://www.raylib.com/examples.html
-*/
 
 #include "raylib.h"
 #include "raymath.h"
@@ -9,51 +5,57 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #include "raygui.h"
 #include "game.h"
 
-const unsigned int TARGET_FPS = 50; //frames/second
-float dt = 1.0f / TARGET_FPS; //seconds/frame
-float time = 0;
-float x = 500;
-float y = 500;
-float frequency = 1;
-float amplitude = 100;
+const int winWidth = 1500;
+const int winHeight = 1000;
 
-//Changes world state
+const unsigned int frameRate = 60;
+float dt = 1.0f / frameRate;
+
+Vector2 launchPos;
+float launchAngle = 0;
+float launchSpeed = 0;
+
+Vector2 position = { 500, 500 };
+Vector2 velocity = { 0, 0 };
+Vector2 accelGrav = { 0, 9 };
+
 void update()
 {
-	dt = 1.0f / TARGET_FPS;
-	time += dt;
+	dt = 1.0f / frameRate;
 
-	x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
-	y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+	if (IsKeyDown(KEY_W)) launchPos.y -= 1;
+	if (IsKeyDown(KEY_S)) launchPos.y += 1;
+	if (IsKeyDown(KEY_A)) launchPos.x -= 1;
+	if (IsKeyDown(KEY_D)) launchPos.x += 1;
 }
 
-//Display world state
-void draw()
+void drawScene() 
 {
 	BeginDrawing();
-	ClearBackground(BLACK);
-	DrawText("Joss Moo-Young 123456789", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
 
+	GuiSliderBar(Rectangle{ 10, 15, 500, 40 }, "", TextFormat("Speed: %.0f", launchSpeed), &launchSpeed, -1000, 1000);
+	GuiSliderBar(Rectangle{ 10, 70, 500, 40 }, "", TextFormat("Angle: %.0f Degrees", launchAngle), &launchAngle, -180, 180);
 
-	GuiSliderBar(Rectangle{ 10, 15, 1000, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
-	DrawText(TextFormat("T: %6.2f", time), GetScreenWidth() - 140, 10, 30, LIGHTGRAY);
+	// Draw scene
+	ClearBackground(SKYBLUE);
+	DrawRectangle(0, GetScreenHeight() - 100, GetScreenWidth(), 100, DARKGREEN);
+	DrawCircleV(launchPos, 50, RED); // Draw bird
 
-	DrawCircle(x, y, 70, RED);
-	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+	Vector2 velocity = { launchSpeed * cos(launchAngle * DEG2RAD), -launchSpeed * sin(launchAngle * DEG2RAD) };
+
+	DrawLineEx(launchPos, launchPos + velocity, 3, RED);
 
 	EndDrawing();
-
 }
 
 int main()
 {
-	InitWindow(InitialWidth, InitialHeight, "GAME2005 Joss Moo-Young 123456789");
-	SetTargetFPS(TARGET_FPS);
+	InitWindow(winWidth, winHeight, "Test");
+	launchPos = { 100, (float)GetScreenHeight() - 100 };
 
-	while (!WindowShouldClose()) // Loops TARGET_FPS times per second
-	{
+	while (!WindowShouldClose()) {
 		update();
-		draw();
+		drawScene();
 	}
 
 	CloseWindow();
